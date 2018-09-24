b9965d022d4c4025581c31cb0163b0fadd0baddb
Seth Beckman <beckmanseth@gmail.com>
2018-09-23 22:34:49 -0400

    PhoneBill stuffs

--- Assignment 3 - Selection Structures/Source/phonebill.java
+++ Assignment 3 - Selection Structures/Source/phonebill.java
@@ -8,27 +8,71 @@ public class phonebill
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Please input the account number: ");
-		int acct = sc.nextInt();
+		int acct = sc.nextInt(); // reads arbitrary account number
 		System.out.print("Please input service code: ");
-		char svc_code = sc.next().charAt(0);
+		char svc_code = sc.next().charAt(0); // reads service code character
 		
+		/* the following blocks check for the appropriate service codes.
+		 * valid codes include p or P for 'Premium' service, and 
+		 * r or R for regular service. if no valid code is read,
+		 * the program exits, informing the user to input a valid code.
+		 */
 		if (svc_code == 'p' || svc_code == 'P')
 		{
+			double svc_fee = 25.00;
 			System.out.print("Please input daytime minutes: ");
 			int day_min = sc.nextInt();
+			if (day_min <= 50)
+			{
+				System.out.println("Account Number: " + acct);
+				System.out.println("Service Type: Premium");
+				System.out.println("Dayttime Minutes: " + day_min);
+			}
+			else if (day_min > 50)
+			{
+				System.out.println("Account Number: " + acct);
+				System.out.println("Service Type: Premium");
+				System.out.println("Dayttime Minutes: " + day_min);
+				day_min = day_min - 50;
+				svc_fee = svc_fee + ((double)day_min * .2); 
+			}
+				
 			System.out.print("Please input nighttime minutes: ");
 			int night_min = sc.nextInt();
-			System.out.println("Service code is " + svc_code + " and this condition works.");
+			if (night_min <= 100)
+				System.out.println("Nighttime Minutes: " + night_min);
+			else if (night_min > 100)
+			{
+				System.out.println("Nighttime Minutes: " + night_min);
+				night_min = night_min - 100;
+				svc_fee = svc_fee + ((double)night_min * .1);
+			}
+			System.out.println("Amount Due: " + svc_fee);		
 		}
 		else if (svc_code == 'r' || svc_code == 'R')
 		{
 			System.out.print("Please input used minutes: ");
 			int mins = sc.nextInt();
-			System.out.println("Service code is " + svc_code + " and this condition works.");
+			
+			if (mins < 50)
+			{
+				double svc_fee = 15.00;
+				System.out.println();
+				System.out.println("Account Number: " + acct);
+				System.out.println("Service Type: Regular");
+				if (mins < 50)
+					System.out.println("Total Minutes: " + mins);
+				else if (mins > 50)
+				{
+					System.out.println("Total Minutes: " + mins);
+					mins = mins - 50;
+					svc_fee = svc_fee + ((double)mins * .5);
+				}	
+			}
 		}
 		else 
 		{
-			System.out.println("Please input a valid security code.");
+			System.out.println("Please input a valid service code.");
 		}
 		System.out.print("That's all for now folks");
 	}
